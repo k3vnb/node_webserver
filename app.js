54f1a3832daa0f78696cc404d167ceb706f2796c
Kevin Boyle <papershack@gmail.com>
2019-02-13 14:57:34 -0800

    refactor for dynamic templating

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ http.createServer((req, res) => {
     //http request starts with whats in the head - status code + headers & MIME types
     res.writeHead(200, { 'Content-Type': 'text/html' });
     // send the body w/ carriage return to represent end of data
-    const html = fs.readFileSync(__dirname + '/index.htm');
+    let html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+    const message = "Hello world...";
+    //dynamic templating
+    html = html.replace('{Message}', message)
     res.end(html);
     // where does Node map to port? the .listen method below
 }).listen(1337, '127.0.0.1');
