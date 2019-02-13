091a53f1e40df58ac8f743d39aee4c3f5ec904c4
Kevin Boyle <papershack@gmail.com>
2019-02-13 14:48:21 -0800

    extrapolate to include html file

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((req, res) => {
     //http request starts with whats in the head - status code + headers & MIME types
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     // send the body w/ carriage return to represent end of data
-    res.end('Hello World\n');
+    const html = fs.readFileSync(__dirname + '/index.htm');
+    res.end(html);
     // where does Node map to port? the .listen method below
 }).listen(1337, '127.0.0.1');
 
-
+// in terminal run $ node app.js
+// 'hello world' will be printed to browser when you go to <localhost:1337>
