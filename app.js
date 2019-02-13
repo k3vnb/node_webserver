564a1b332c3c8df065fe5c5d0a804fcbb15c3236
Kevin Boyle <papershack@gmail.com>
2019-02-13 14:32:33 -0800

    complete basic node server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const http = require('http');
+
+http.createServer((req, res) => {
+    //http request starts with whats in the head - status code + headers & MIME types
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // send the body w/ carriage return to represent end of data
+    res.end('Hello World\n');
+    // where does Node map to port? the .listen method below
+}).listen(1337, '127.0.0.1');
+
+
