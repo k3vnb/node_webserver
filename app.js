13d35759f55f0b79fdc661942d139006fb8b1ebf
Kevin Boyle <papershack@gmail.com>
2019-02-14 17:24:13 -0800

    add routing w/ param matching via :, ie /person/:id

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.send(`<html><head></head><body><h1>HelloWorld!</h1></body>`)
-})
+    res.send(`<html><head></head><body><h1>HelloWorld!</h1></body>`);
+});
+
+app.get('/person/:id', function(req, res) {
+    res.send(`<html><head></head><body><h1>Person: ${req.params.id}</h1></body>`);
+});
+
+app.get('/api', function(req, res) {
+    res.json({ firstname: 'John', lastname: 'Doe'});
+});
 
 app.listen(port);
 
