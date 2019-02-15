dcee64c804eaa17fddb088ea10d5b0a7d9a8f838
Kevin Boyle <papershack@gmail.com>
2019-02-14 16:26:11 -0800

    add error404 page to routing

--- app.js
+++ app.js
@@ -4,21 +4,25 @@ const fs = require('fs');
 http.createServer((req, res) => {
 
     if (req.url === '/'){
-    //http request starts with whats in the head - status code + headers & MIME types
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    // send the body w/ carriage return to represent end of data
-    fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
-    }
-    
-    if (req.url === '/api'){
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    const obj = {
-        firstname: 'John',
-        lastname: 'Doe'
-    };
-    res.end(JSON.stringify(obj));
+        //http request starts with whats in the head - status code + headers & MIME types
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        // send the body w/ carriage return to represent end of data
+        fs.createReadStream(__dirname + '/index.htm', 'utf8').pipe(res);
     }
 
+    else if (req.url === '/api'){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        const obj = {
+            firstname: 'John',
+            lastname: 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    }
+
+   else { res.writeHead(404, { 'Content-Type': 'text/html' });
+   // send the body w/ carriage return to represent end of data
+   fs.createReadStream(__dirname + '/error404.htm', 'utf8').pipe(res);;
+   }
 
 }).listen(1337, '127.0.0.1');
 
